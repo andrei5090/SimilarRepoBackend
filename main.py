f6a795c7a1676e8b38850304003198998147e91a
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-05-27 01:27:27 +0200

    get full hierarchy from backend

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+
 from util.hierarchy import get_hierarchy, get_available_tags
 
-
 app = FastAPI()
-
 
 origins = [
     "http://localhost:8080",
@@ -23,11 +22,42 @@ app.add_middleware(
 # method='ward',
 
 @app.get("/hierarchy")
-async def generate_hierarchy(cuts=None, method: str = 'ward', metric: str = 'euclidean'):
+async def generate_hierarchy(cuts: int = None, method: str = 'ward', metric: str = 'euclidean'):
     if cuts is None:
         cuts = [520]
 
-    return get_hierarchy(cuts, method, metric)
+    cuts_list = []
+    if cuts == 0 or cuts == 1:
+        cuts_list.append(1)
+    else:
+        while cuts > 0:
+            if 200 < cuts < 600:
+                cuts -= 15
+            elif 100 < cuts < 200:
+                cuts -= 10
+            elif 50 < cuts < 100:
+                cuts -= 5
+            elif 10 < cuts < 50:
+                cuts -= 3
+            elif cuts < 10:
+                cuts -= 1
+            else:
+                cuts -= 1
+
+            if cuts <= 0:
+                break
+
+            cuts_list.append(cuts)
+
+    val = None
+
+    try:
+        val = get_hierarchy(cuts_list, method, metric)
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=e.__str__())
+
+    # print(cuts_list)
+    return val
 
 
 @app.get("/tags")
