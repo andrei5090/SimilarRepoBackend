8599f0846cfa13d51752b8f20cedbd389799743c
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-06-06 00:36:24 +0200

    make it better

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ async def generate_hierarchy(cuts: int = None, method: str = 'ward', metric: str
 @app.get("/tags")
 @cache(expire=86400)
 async def generate_hierarchy():
+
     return get_available_tags()
 
 
