ad8ea52b73e632f114337f83771cb2a2e49059eb
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-06-11 00:10:39 +0200

    add time statistics

--- util/statistics.py
+++ util/statistics.py
@@ -1,7 +1,7 @@
 from schemas import Feedback, CreateAndUpdateFeedback
 
 
-def convertMillis(millis):
+def convertInTime(millis):
     seconds = (millis / 1000) % 60
     minutes = (millis / (1000 * 60)) % 60
     hours = (millis / (1000 * 60 * 60)) % 24
@@ -10,7 +10,8 @@ def convertMillis(millis):
 
 def buildStatistics(data):
     userData = {}
-    res = {}
+    userStatistics = {}
+    scenarioStatistics = {}
 
     for feedback in data:
         key = feedback.extraInfo['userId']
@@ -22,26 +23,39 @@ def buildStatistics(data):
 
     for key in userData:
         userData[key].sort(key=lambda x: x.extraInfo['scenarioId'])
-        res[key] = {}
+        userStatistics[key] = {}
         for i in range(0, len(userData[key])):
-            res[key][userData[key][i].extraInfo['scenarioId']] = {}
+            userStatistics[key][userData[key][i].extraInfo['scenarioId']] = {}
 
     totalAvg = 0
     for key in userData:
         avg = 0
         for i in range(0, len(userData[key])):
+            time = 0
+            id = userData[key][i].extraInfo['scenarioId']
+
             if i == 0:
                 time = userData[key][0].extraInfo['endTime'] - userData[key][0].extraInfo['startTimeTask']
-                res[key][userData[key][i].extraInfo['scenarioId']]['time'] = convertMillis(time)
+                userStatistics[key][id]['time'] = convertInTime(time)
                 avg += time
             else:
                 time = userData[key][i].extraInfo['endTime'] - userData[key][i - 1].extraInfo['endTime']
-                res[key][userData[key][i].extraInfo['scenarioId']]['time'] = convertMillis(time)
+                userStatistics[key][id]['time'] = convertInTime(time)
                 avg += time
 
-        res[key]['averageTimeSpentPerTask'] = convertMillis(avg / len(userData[key]))
+            if id not in scenarioStatistics:
+                scenarioStatistics[id] = {'averageTime': 0, 'noOfAnswers': 0}
+
+            scenarioStatistics[id]['averageTime'] = scenarioStatistics[id]['averageTime'] + time
+            scenarioStatistics[id]['noOfAnswers'] = scenarioStatistics[id]['noOfAnswers'] + 1
+
+        userStatistics[key]['averageUserTime'] = convertInTime(avg / len(userData[key]))
         totalAvg += avg
 
-    res['totalAverageTimeSpent'] = convertMillis(totalAvg / len(userData.keys()))
+    userStatistics['totalAverageTimeSpentOnEvaluation'] = convertInTime(totalAvg / len(userData.keys()))
 
-    return res
+    for key in scenarioStatistics:
+        scenarioStatistics[key]['averageTime'] = convertInTime(
+            scenarioStatistics[key]['averageTime'] / scenarioStatistics[key]['noOfAnswers'])
+
+    return {'userStatistics': userStatistics, 'scenarioStatistics': scenarioStatistics}
