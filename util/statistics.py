0d751171ace782e807eeeac7b657cfc5fe63a3cc
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-06-10 17:17:38 +0200

    add statistics

--- util/statistics.py
+++ util/statistics.py
@@ -5,7 +5,7 @@ def convertMillis(millis):
     seconds = (millis / 1000) % 60
     minutes = (millis / (1000 * 60)) % 60
     hours = (millis / (1000 * 60 * 60)) % 24
-    return "{0} hours {1} minutes {2} seconds".format(hours, minutes, seconds)
+    return "{0} hours {1} minutes {2} seconds".format(round(hours), round(minutes), round(seconds))
 
 
 def buildStatistics(data):
@@ -22,11 +22,26 @@ def buildStatistics(data):
 
     for key in userData:
         userData[key].sort(key=lambda x: x.extraInfo['scenarioId'])
+        res[key] = {}
+        for i in range(0, len(userData[key])):
+            res[key][userData[key][i].extraInfo['scenarioId']] = {}
 
+    totalAvg = 0
     for key in userData:
-        res[key] = [convertMillis(x['endTime'] - x['startTime']) for x in userData[key]['extraInfo']]
+        avg = 0
+        for i in range(0, len(userData[key])):
+            if i == 0:
+                time = userData[key][0].extraInfo['endTime'] - userData[key][0].extraInfo['startTimeTask']
+                res[key][userData[key][i].extraInfo['scenarioId']]['time'] = convertMillis(time)
+                avg += time
+            else:
+                time = userData[key][i].extraInfo['endTime'] - userData[key][i - 1].extraInfo['endTime']
+                res[key][userData[key][i].extraInfo['scenarioId']]['time'] = convertMillis(time)
+                avg += time
 
-    print("ressss ")
-    print(res)
+        res[key]['averageTimeSpentPerTask'] = convertMillis(avg / len(userData[key]))
+        totalAvg += avg
+
+    res['totalAverageTimeSpent'] = convertMillis(totalAvg / len(userData.keys()))
 
     return res
