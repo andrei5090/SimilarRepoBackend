613ba27583f56bc48de30c5db655308db6028f77
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2022-06-13 15:03:11 +0200

    fix api

--- util/google.py
+++ util/google.py
@@ -3,12 +3,12 @@ import requests
 API_KEY = "AIzaSyB81vXMOsNOF-xRo_Lh7NXKaA3u9BQgWqA"
 SEARCH_ENGINE_ID = "5ab9439e0450be483"
 
-
+#https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyB81vXMOsNOF-xRo_Lh7NXKaA3u9BQgWqA&cx=5ab9439e0450be483&q=test&start=1
 def get_start_no(page):
     return (page - 1) * 10 + 1
 
 
-def google_search(query, pages=5):
+def google_search(query, pages=7):
     global API_KEY
     global SEARCH_ENGINE_ID
     res = []
@@ -19,6 +19,7 @@ def google_search(query, pages=5):
         data = requests.get(url).json()
 
         search_items = data.get("items")
+
         for i, search_item in enumerate(search_items, start=1):
             try:
                 long_description = search_item["pagemap"]["metatags"][0]["og:description"]
@@ -37,7 +38,7 @@ def google_search(query, pages=5):
 
     res_set = []
     for i in res:
-        if i.count('/') == 4 and i.count('?') == 0 and i.count('https://github.com') == 1 and i.count("topics") == 0:
+        if i.count('/') == 4 and i.count('?') == 0 and i.count('https://github.com') == 1 and i.count("topics") == 0 and i.count("https://github.com/blog/") == 0:
             res_set.append(i)
 
     return {'links': res_set}
